677533b001a785dc4d0589722ed66370b0fa494e
Antonio Martín <ajms66@gmail.com>
2022-03-14 17:58:51 +0100

    proyectos 1 al 8

---
+++ 08_academia_web_confg_clases/src/main/java/com/sanitas/dao/AlumnosDaoImpl.java
@@ -0,0 +1,70 @@
+package com.sanitas.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.sanitas.model.Alumno;
+@Repository
+public class AlumnosDaoImpl implements AlumnosDao {
+	@PersistenceContext
+	EntityManager entityManager;
+	@Transactional
+	@Override
+	public void save(Alumno al) {
+		entityManager.persist(al);
+
+	}
+	@Transactional
+	@Override
+	public void remove(Alumno al) {
+		entityManager.remove(al);
+
+	}
+
+	@Override
+	public Alumno findById(int idAlumno) {
+		return entityManager.find(Alumno.class, idAlumno);
+	}
+
+	@Override
+	public Alumno findByNombre(String nombre) {
+		String jpql="Select a from Alumno a where a.nombre=?1";
+		TypedQuery<Alumno> tquery=entityManager.createQuery(jpql,Alumno.class);
+		tquery.setParameter(1, nombre);
+		/*List<Alumno> alumnos=tquery.getResultList();
+		return alumnos.size()>0?alumnos.get(0):null;*/
+		try {
+			return tquery.getSingleResult();
+		}
+		catch(NonUniqueResultException|NoResultException ex) {
+			return null;
+		}
+	}
+
+	@Override
+	public List<Alumno> findByCurso(String curso) {
+		String jpql="Select a from Alumno a where a.curso=?1";
+		TypedQuery<Alumno> tquery=entityManager.createQuery(jpql,Alumno.class);
+		tquery.setParameter(1, curso);
+		return tquery.getResultList();
+		
+	}
+	@Transactional
+	@Override
+	public void deleteByCurso(String curso) {
+		String jpql="delete from Alumno a where a.curso=?1";
+		Query query=entityManager.createQuery(jpql);
+		query.setParameter(1, curso);
+		query.executeUpdate();
+	}
+
+}
---
+++ 08_academia_web_confg_clases/src/main/webapp/datos.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="alta" method="post">
+		Nombre: <input type="text" name="nombre"><br>
+		Curso: <input type="text" name="curso"><br>
+		Nota: <input type="text" name="nota"><br>
+		<input type="submit" value="Guardar"> 
+	</form>
+</body>
+</html>
