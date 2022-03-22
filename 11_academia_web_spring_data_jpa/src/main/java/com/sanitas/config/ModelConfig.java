0487f0c447e9a7d0a40b4c86308e486501141c38
Antonio Martín <ajms66@gmail.com>
2022-03-22 15:28:15 +0100

    ejercicio 11

---
+++ 11_academia_web_spring_data_jpa/src/main/java/com/sanitas/config/ModelConfig.java
@@ -0,0 +1,66 @@
+package com.sanitas.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+@PropertySource("classpath:com/sanitas/config/application.properties")
+@EnableTransactionManagement
+@ComponentScan(basePackages = {"com.sanitas.service"})
+@EnableJpaRepositories(basePackages = {"com.sanitas.dao"}, 
+						entityManagerFactoryRef = "factory",
+						transactionManagerRef = "txManager")
+@Configuration
+public class ModelConfig {
+	@Value("${driver}")
+	String driver;
+	@Value("${url}")
+	String url;
+	@Value("${user}")
+	String user;
+	@Value("${pwd}")
+	String pwd;
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource data=new DriverManagerDataSource();
+		data.setDriverClassName(driver);
+		data.setUrl(url);
+		data.setUsername(user);
+		data.setPassword(pwd);
+		return data;
+	}
+	//adaptador de Hibernate
+		@Bean
+		public HibernateJpaVendorAdapter adapter() {
+			HibernateJpaVendorAdapter adp=new HibernateJpaVendorAdapter();
+			adp.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+			return adp;
+		}
+		//factoria EntityManager: Objeto para acceder a capa de persistencia con JPA
+		@Bean
+		public LocalContainerEntityManagerFactoryBean factory(DataSource dataSource, HibernateJpaVendorAdapter adapter) {
+			LocalContainerEntityManagerFactoryBean factory=new LocalContainerEntityManagerFactoryBean();
+			factory.setPersistenceUnitName("academiaPU");
+			factory.setDataSource(dataSource);
+			factory.setPackagesToScan("com.sanitas.model");
+			factory.setJpaVendorAdapter(adapter);
+			return factory;
+		}
+		
+		//gestor de transacción
+		@Bean
+		public JpaTransactionManager txManager(LocalContainerEntityManagerFactoryBean factory) {
+			JpaTransactionManager manager=new JpaTransactionManager();
+			manager.setEntityManagerFactory(factory.getObject());
+			return manager;
+		}
+}
---
+++ 11_academia_web_spring_data_jpa/src/main/java/com/sanitas/controller/AlumnosController.java
@@ -0,0 +1,52 @@
+package com.sanitas.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.sanitas.model.Alumno;
+import com.sanitas.service.AlumnosService;
+@CrossOrigin("*")
+@Controller
+public class AlumnosController {
+	@Autowired
+	AlumnosService alumnosService;
+	@PostMapping("alta")
+	public String altaAlumno(@ModelAttribute("alumno") Alumno alumno, 
+			HttpServletRequest request) {		
+		if(alumnosService.altaAlumno(alumno)) {
+			request.setAttribute("mensaje", "Alumno agregado");
+		}else {
+			request.setAttribute("mensaje", "El alumno no se pudo añadir");
+		}
+		return "inicio";
+	}
+	/*public String altaAlumno(@RequestParam("nombre") String nombre, 
+							@RequestParam("curso") String curso, 
+							@RequestParam("nota") double nota, 
+							HttpServletRequest request) {
+		Alumno alumno=new Alumno(nombre, curso, nota);
+		if(alumnosService.altaAlumno(alumno)) {
+			request.setAttribute("mensaje", "Alumno agregado");
+		}else {
+			request.setAttribute("mensaje", "El alumno no se pudo añadir");
+		}
+		return "inicio";
+	}*/
+	
+	
+	@GetMapping(value="alumnos",produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Alumno> listadoAlumnos(@RequestParam("curso") String curso) {
+		return alumnosService.alumnosCurso(curso);
+	}
+}
