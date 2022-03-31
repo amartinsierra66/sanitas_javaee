bc8271ab09c1acd16f1d0f577dde167c422c8b41
Antonio Martín <ajms66@gmail.com>
2022-03-31 16:01:40 +0200

    commit 19

--- 19_banca_spring_boot/src/test/java/com/sanitas/init/ApplicationTests.java
+++ 19_banca_spring_boot/src/test/java/com/sanitas/init/ApplicationTests.java
@@ -1,13 +1,19 @@
 package com.sanitas.init;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.sanitas.service.CajeroService;
 
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test() {
+		
 	}
 
 }
